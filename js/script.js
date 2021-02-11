cf2c999be1b76702079c74608817bfe09b472af0
akash prasher <prince.akash.prasher@gmail.com>
2021-02-11 13:28:22 +0530

    Adding Canvas and Expression stuff
    Signed-off-by: akash prasher <prince.akash.prasher@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-const vido = document.getElementById("video");
+const video = document.getElementById("video");
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri("./models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("./models"),
+]).then(startVideo);
 
 function startVideo() {
   navigator.getUserMedia(
@@ -10,4 +17,19 @@ function startVideo() {
   );
 }
 
-startVideo()
+video.addEventListener("play", () => {
+  const canvas = faceapi.createCanvasFromMedia(video)
+  document.body.append(canvas)
+  const displaySize = { width: video.width, height: video.height }
+  faceapi.matchDimensions(canvas, displaySize)
+  setInterval(async () => {
+    const detections = await faceapi
+      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks()
+      .withFaceExpressions();
+    console.log(detections)
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    faceapi.draw.drawDetections(canvas, resizedDetections)
+  }, 100);
+});
