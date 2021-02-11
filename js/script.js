5cc7c697fe0285066899bf988be756f1ae8c564f
akash prasher <prince.akash.prasher@gmail.com>
2021-02-11 13:32:40 +0530

    Adding Landmarks and expressions
    Signed-off-by: akash prasher <prince.akash.prasher@gmail.com>

--- js/script.js
+++ js/script.js
@@ -27,9 +27,11 @@ video.addEventListener("play", () => {
       .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
       .withFaceLandmarks()
       .withFaceExpressions();
-    console.log(detections)
+    // console.log(detections)
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     faceapi.draw.drawDetections(canvas, resizedDetections)
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
   }, 100);
 });
