4995194258d64f1e7d41d2592cd90a8013aa7706
akash prasher <prince.akash.prasher@gmail.com>
2021-02-11 12:30:31 +0530

    Importinf video by id and fn-startVideo
    Signed-off-by: akash prasher <prince.akash.prasher@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const vido = document.getElementById("video");
+
+function startVideo() {
+  navigator.getUserMedia(
+    {
+      video: {},
+    },
+    (stream) => (video.srcObject = stream),
+    (err) => console.error(err)
+  );
+}
+
+startVideo()
